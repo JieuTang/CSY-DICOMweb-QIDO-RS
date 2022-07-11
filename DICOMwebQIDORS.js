1137d61d5bff93ff95e1379c8aa4f15ab643df05
JieuTang <jieutang@gmail.com>
2022-07-11 20:58:45 +0800

    docs: add class DICOMwebQIDORS

---
+++ DICOMwebQIDORS.js
@@ -0,0 +1,15 @@
+import URL from "url-parse";
+
+class DICOMwebQIDORS {
+    constructor() {
+
+    }
+
+    async init() {
+
+    }
+}
+
+
+
+export default DICOMwebQIDORS;
--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+import QIDO from "./DICOMwebQIDORS.js";
+
+let qido = new QIDO();
+
 (async function () {
     console.log("Hello World!");
+    console.log(qido);
 })();
 
 
 
-// export default {};
