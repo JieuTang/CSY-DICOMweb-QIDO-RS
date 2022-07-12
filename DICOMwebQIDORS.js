6f90a6002bc2ab74eff580060f6fea3a9351ea3f
JieuTang <jieutang@gmail.com>
2022-07-12 12:56:11 +0800

    refactor: build DICOMwebQIDORS class structure

--- DICOMwebQIDORS.js
+++ DICOMwebQIDORS.js
@@ -1,11 +1,50 @@
 import URL from "url-parse";
+import _ from "lodash";
+import QIDOParameter from "./QIDOParameter.json" assert {type: "json"};
 
 class DICOMwebQIDORS {
     constructor() {
+        //QIDOParameter.json
+        this.terminologyList = undefined;
+        this.parameterList = undefined;
+        
+        //Token
+        this.isUseToken = false;
+        this.tokenValue = undefined;
+
+        //url-parse package
+        this.hostname = undefined;
+        this.pathname = undefined;
+        this.port = undefined;
+        this.protocol = undefined;
+        this.queryParameter = undefined
+    }
+
+    async _init() {
+        await _loadQIDOParameter();
+    }
+
+    async _loadQIDOParameter() {
 
     }
 
-    async init() {
+    async setParameter() {
+
+    }
+
+    async query() {
+
+    }
+
+    async setUseToken(isUseToken = true, tokenValue) {
+
+    }
+
+    async _validateTokenCanUse() {
+        
+    }
+
+    async _validateUrlComponent() {
 
     }
 }
