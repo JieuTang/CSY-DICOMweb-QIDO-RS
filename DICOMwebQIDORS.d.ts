b8ef4fd86c1a7f9cc3ab4e702b3b6ad2fc85cb1e
JieuTang <jieutang@gmail.com>
2022-07-28 13:36:46 +0800

    feat: add TypeScript Declaration File

---
+++ DICOMwebQIDORS.d.ts
@@ -0,0 +1,29 @@
+export default class DICOMwebQIDORS {
+    terminologyList: Object | undefined;
+    studyParameterList: Object | undefined;
+    seriesParameterList: Object | undefined;
+    instanceParameterList: Object | undefined;
+
+    //Token
+    isUseToken: Boolean;
+    tokenObject: Object | undefined;
+
+    //url-parse package
+    hostname: string | undefined;
+    pathname: string | undefined;
+    port: string | undefined;
+    protocol: string | undefined;
+    queryParameter: Object | undefined
+
+    //Query Level
+    queryLevel: string | undefined;
+
+    //Response
+    response: Object | undefined;
+
+   async init(): void;
+   
+   async query(): void;
+   
+   async setUseToken(tokenObject: Object): void;
+}
